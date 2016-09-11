5f69f3c2d86901353135ce59b1252524643bcf2b
Francisco Quiñones Enciso <joseq@apesol.org.pe>
2016-09-10 22:53:17 -0500

    change host server

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {};
 if(process.env.NODE_ENV == "pro"){
   config = {
     server: {
-      host: 'localhost',
+      host: '0.0.0.0',
       port: process.env.PORT || 3000
     }
   }
@@ -10,7 +10,7 @@ if(process.env.NODE_ENV == "pro"){
 if (process.env.NODE_ENV == 'dev' || !process.env.NODE_ENV) {
   config = {
     server: {
-      host: 'localhost',
+      host: '0.0.0.0',
       port: process.env.PORT || 3010
     }
   }
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV == 'dev' || !process.env.NODE_ENV) {
 if (process.env.NODE_ENV == 'test') {
   config = {
     server: {
-      host: 'localhost',
+      host: '0.0.0.0',
       port: process.env.PORT || 3020
     },
   }
